8aabe5b5f2b9fbde5fbd9e1b1c62cae1954b8c81
Jordan Binder <binder.jordan@gmail.com>
2021-02-07 23:03:28 -0800

    testing modules and small change to app.js

--- project_2/app.js
+++ project_2/app.js
@@ -1,3 +1,7 @@
+const path require('path'); // node assumes this is built in module.
+// code like require('./path') will make Node.js look for this file within the scope of the directory
+let pathObj = path.parse(__filename);
+
 const LOG = require('./logger')
 
 LOG('message');
--- project_2/logger.js
+++ project_2/logger.js
@@ -1,9 +1,12 @@
-let url = 'https://mylogger.io/log'; // sends h
 
-function log(message) {
-  // send http request
-  console.log(message);
-}
+  let url = 'https://mylogger.io/log'; // sends h
 
-module.exports = log; // This is what allows you to call that variable or function outside the scope of the file
-module.exports.log /* this defines the object as log */ = log // this option is for if he have multiple methods in this file;
+  function log(message) {
+    // send http request
+    console.log(message);
+  }
+  
+  module.exports = log; // This is what allows you to call that variable or function outside the scope of the file
+  module.exports.log /* this defines the object as log */ = log // this option is for if he have multiple methods in this file;
+
+// Module wrapper is happening under the hood
